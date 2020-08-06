f04634bc195bfa3f682f205bf77c6bc3cd2f1d05
Ignats Fjodorovs <ignatsfjodorovs@gmail.com>
2020-08-06 23:58:56 +0300

    - Added popup for future configurations - Started creating translator popup after text selection

--- content/main.js
+++ content/main.js
@@ -1,15 +1,36 @@
 function createPopUp() {
     var popUp = document.createElement('div');
-    popUp.className = 'popup-container';
+    popUp.id = 'popup-container';
     document.body.appendChild(popUp);
 }
 
+// Return selection object
 function getSelected() {
     if (window.getSelection) {
         return window.getSelection;
     }
 
     return false;
+}
+
+// temporary function to create popup and style it
+function intializeTranslatorPopup() {
+    createPopUp();
+    styles = document.createElement("link");
+    styles.setAttribute("href", chrome.runtime.getURL("translator_popup.css"));
+    styles.setAttribute("rel", "stylesheet");
+    styles.setAttribute("type", "text/css");
+    document.head.appendChild(styles);
+    popup = document.getElementById('popup-container');
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            popup.innerHTML = this.responseText;
+        }
+    };
+    xhttp.open("GET", chrome.runtime.getURL('translator_popup.html'), true);
+    xhttp.send();
 }
 
 function popUpTemplate(data) {
@@ -23,13 +44,19 @@ function popUpTemplate(data) {
     `;
 }
 
+// Add listener on mouseup to body when page is loaded
 document.getElementsByTagName('body')[0].addEventListener('mouseup', function (event) {
     let selection = getSelected().toString().trim();
     let numbers = /^[0-9]+$/;
 
     if (selection && !selection.match(numbers)) {
         console.log(selection);
+        popup = document.getElementById('popup-container');
+        console.log(event.clientY);
+        console.log(event.clientX);
+        popup.style.cssText = `display: block; top: ${event.clientY}px; left: ${event.clientX}px;`;
     }
     
 });
 
+intializeTranslatorPopup();
